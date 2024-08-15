914cbf3281fb3313987304c079e4b600877d2a38
Lubasha <parakhonya1@yandex.ru>
2024-08-15 22:20:43 +0300

    rout

--- vite-project/src/component/Card/Card.jsx
+++ vite-project/src/component/Card/Card.jsx
@@ -2,19 +2,18 @@ import React, { useState, useEffect } from 'react';
 import Item from '../Item/Item'
 import style from './Card.module.css'
 import { RiArrowGoBackLine } from "react-icons/ri";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import uuid from 'react-uuid';
+
 
 
 const Card = ({task}) => {
   const history = useNavigate()
+  const cardId = useParams().id
  
-    const currentUrl = window.location.pathname
-    const lengthUrl = currentUrl.length
-    const idPost = parseInt(currentUrl.slice(6, lengthUrl))
   return (
     <div className={style.cardMain} >
-    <Item className={style.test} key={uuid()} id={task[idPost-1].id} title={task[idPost-1].title} isCompleted={task[idPost-1].completed}/>
+    <Item className={style.test} key={uuid()}  title={task[cardId-1].title} isCompleted={task[cardId-1].completed}/>
     <RiArrowGoBackLine className={style.backButton} onClick={()=>history('/')}/>
     </div> 
     
@@ -24,3 +23,12 @@ const Card = ({task}) => {
 export default Card
 
 
+// const currentUrl = window.location.pathname
+//     const lengthUrl = currentUrl.length
+//     const idPost = parseInt(currentUrl.slice(6, lengthUrl))
+//     console.log(idPost)
+
+{/* <div className={style.cardMain} >
+    <Item className={style.test} key={uuid()} id={task[idPost-1].id} title={task[idPost-1].title} isCompleted={task[idPost-1].completed}/>
+    <RiArrowGoBackLine className={style.backButton} onClick={()=>history('/')}/>
+    </div>  */}
